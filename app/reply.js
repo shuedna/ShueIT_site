71933df254e780973be04aaad0e1d54c92956122
Daniel Shue <shuedna@gmail.com>
2017-06-04 20:48:29 -0400

    fix cache max-age header

--- app/reply.js
+++ app/reply.js
@@ -12,15 +12,15 @@ function send (request, response, data) {
 		consolelogs('gzip')
 		zlib.gzip(data.contents, function(error, result){
 			if (error) {
-				response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public;max-age:86400"})
+				response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public","Cache-Control":"max-age=86400"})
 				response.end(data.contents)
 			}else{
-				response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public;max-age:86400","Content-Encoding":"gzip"})
+				response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public","Cache-Control":"max-age=86400","Content-Encoding":"gzip"})
 				response.end(result)
 			}
 		})	
 	}else{
-		response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public;max-age:86400"})
+		response.writeHead(data.head, {"Content-Type":data.type,"Cache-Control":"public","Cache-Control":"max-age=86400"})
 		response.end(data.contents)
 	}
 }
